c3b70b6284b0795c16e66351695566c207b56624
dan-araujo <daniel.ti.info@gmail.com>
2022-11-03 14:05:22 -0300

    Class bootcamp created

---
+++ src/com/bootcamp/domain/Bootcamp.java
@@ -0,0 +1,69 @@
+package com.bootcamp.domain;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+public class Bootcamp {
+    public String name;
+    public String description;
+    public final LocalDate initialDate = LocalDate.now();
+    public final LocalDate finalDate = initialDate.plusDays(50);
+    private Set<Dev> subscribedDevs = new HashSet<>();
+    private Set<Contents> contents = new LinkedHashSet<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getInitialDate() {
+        return initialDate;
+    }
+
+    public LocalDate getFinalDate() {
+        return finalDate;
+    }
+
+    public Set<Dev> getSubscribedDevs() {
+        return subscribedDevs;
+    }
+
+    public void setSubscribedDevs(Set<Dev> subscribedDevs) {
+        this.subscribedDevs = subscribedDevs;
+    }
+
+    public Set<Contents> getContents() {
+        return contents;
+    }
+
+    public void setContents(Set<Contents> contents) {
+        this.contents = contents;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Bootcamp bootcamp = (Bootcamp) o;
+        return Objects.equals(name, bootcamp.name) && Objects.equals(description, bootcamp.description) && Objects.equals(initialDate, bootcamp.initialDate) && Objects.equals(finalDate, bootcamp.finalDate) && Objects.equals(subscribedDevs, bootcamp.subscribedDevs) && Objects.equals(contents, bootcamp.contents);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, description, initialDate, finalDate, subscribedDevs, contents);
+    }
+}
